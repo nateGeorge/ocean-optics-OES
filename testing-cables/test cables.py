8b8179a8ed91042eb9bc97bd2f30fd9dfdb2b64d
Nate George <wordsforthewise@gmail.com>
2015-07-16 11:03:35 -0700

    test cables now cycles through all the active zones on the respective tool

--- testing-cables/test cables.py
+++ testing-cables/test cables.py
@@ -10,19 +10,26 @@ import seabreeze
 seabreeze.use('pyseabreeze')
 import seabreeze.spectrometers as sb
 import pylab as plt
-import ctypes, time, matplotlib
+import ctypes, time, matplotlib, sys, os, re
 import numpy as np
 import matplotlib.animation as animation
 import easygui as eg
+sys.path.append("Y:/Nate/git/nuvosun-python-lib/")
+import nuvosunlib as nsl
 
-multiplexComPort = 15
+files = os.listdir('C:/OESdata')
+for file in files:
+    if re.search('MC\d\d.txt',file):
+        tool = file[:4]
+
+BEzoneList, PCzoneList, zoneToIndexMap, MPcomPort = nsl.load_OES_config(tool)
 multiNotConnected = True
 
-matplotlib.rcParams.update({'font.size': 40})
+matplotlib.rcParams.update({'font.size': 50})
 
 mpdll = ctypes.WinDLL('Y:\Nate\Wayne\MPM2000drv.dll')
 
-a = mpdll.MPM_OpenConnection('COM' + str(multiplexComPort)) #OES multiplexer is COM1 for now
+a = mpdll.MPM_OpenConnection(str(MPcomPort)) #OES multiplexer is COM1 for now
 
 print a
 
@@ -32,15 +39,13 @@ print b
 
 while multiNotConnected:
     if a != 1 or b != 1:
-        openDebug = eg.msgbox('Cannot connect to multiplexer (try unplugging the usb cable and plugging the multiplexer usb cable back in).  Multiplexer is set as COM' + str(multiplexComPort) + '.')
+        openDebug = eg.msgbox('Cannot connect to multiplexer (try unplugging the usb cable and plugging the multiplexer usb cable back in).  Multiplexer is set as ' + str(MPcomPort) + '.')
         exit()
     else:
         print 'successfully connected to multiplexer'
         multiNotConnected = False
 # if need to select between multiple multiplexers, use this line:
 # serialNo = mpdll.MPM_GetSerialNumber() #giving 0 for both multiplexers...not sure if this is correct.  OES multiplexer is COM1
-
-mpdll.MPM_SetChannel(2)
 
 #connects to first connected spectrometer it finds, takes intTime as integration time in nanoseconds, darkChannel as 
 #multiplexer channel that is blocked from all light
@@ -59,6 +64,13 @@ spec.integration_time_micros(1000000) #actually in nanoseconds not microseconds.
 time.sleep(2)
 
 def plotdata(*args):
+    global zoneCount
+    print 
+    if zoneCount > len(zoneList)-1:
+        zoneCount = 0
+    zone = zoneList[zoneCount]
+    mpdll.MPM_SetChannel(zoneToIndexMap[zone])
+    time.sleep(0.5)
     int1 = spec.intensities()
     wl = spec.wavelengths()
     ax = plt.subplot(1,1,1)
@@ -73,7 +85,10 @@ def plotdata(*args):
     plt.plot(plotWl, np.array(plotInt))
     plt.xlim([xLimMin,xLimMax])
     plt.ylim([min(plotInt),max(plotInt)])
+    plt.title('zone ' + zone)
+    zoneCount += 1
     
+        
 fig=plt.figure()
 try:
     figManager = plt.get_current_fig_manager()
@@ -81,8 +96,11 @@ try:
 except AttributeError:
     mng = plt.get_current_fig_manager()
     mng.window.state('zoomed')
-    
 
-ani = animation.FuncAnimation(fig, plotdata, interval=1000)
+global zoneCount
+zoneCount = 0
+zoneList = [zone for zone in sorted(PCzoneList + BEzoneList)]
+
+ani = animation.FuncAnimation(fig, plotdata, interval=1500)
 plt.show()
     
