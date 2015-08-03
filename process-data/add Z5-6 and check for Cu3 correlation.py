d80de6fbb846fb9d8937354689337e2880273fdf
Nate George <wordsforthewise@gmail.com>
2015-08-03 13:04:12 -0700

    working towards adding Z5A+B

--- process-data/add Z5-6 and check for Cu3 correlation.py
+++ process-data/add Z5-6 and check for Cu3 correlation.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 
 #effFile = 'Y:/Nate/OES/databases/OES-XRF-eff-data.csv'
@@ -8,8 +9,16 @@ processedPCfile = 'Y:/Nate/OES/databases/up to 450/PROCESSED all OES data PC - w
 
 oesDF = pd.DataFrame.from_csv(processedPCfile)
 
-print oesDF.columns
-oesByRun = oesDF.groupby(['substrate','zone'])
+#print oesDF.columns
+oesByRun = oesDF.groupby(['substrate'])
 for name, group in oesByRun:
-    print name
-    print name[1]
+    runData = group.groupby(['zone'])
+    sumZ5 = runData['5A'] + runData['5B']
+    for zone, grp in sumZ5:
+        print zone
+        for each in grp:
+            print each
+            if each == 'Cu-515':
+                for row in grp[each]:
+                    print row
+                    time.sleep(0.3)
