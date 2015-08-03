702429ec46aff6d3f69fc3461cde7dc05e87023f
Nate George <wordsforthewise@gmail.com>
2015-08-03 10:53:10 -0700

    added file names, inspect data structs

--- process-data/add Z5-6 and check for Cu3 correlation.py
+++ process-data/add Z5-6 and check for Cu3 correlation.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-effFile = 'Y:/Nate/OES/databases/OES-XRF-eff-data.csv'
+#effFile = 'Y:/Nate/OES/databases/OES-XRF-eff-data.csv'
+#processedPCfile = 'Y:/Nate/OES/databases/PROCESSED all OES data PC - with normalizations.csv'
+effFile = 'Y:/Nate/OES/databases/up to 450/OES-XRF-eff-data.csv'
+processedPCfile = 'Y:/Nate/OES/databases/up to 450/PROCESSED all OES data PC - with normalizations and XRF.csv'
 
-oesDF = pd.DataFrame.from_csv(effFile)
+
+oesDF = pd.DataFrame.from_csv(processedPCfile)
+
+print oesDF.columns
+oesByRun = oesDF.groupby(['substrate','zone'])
+for name, group in oesByRun:
+    print name
+    print name[1]
