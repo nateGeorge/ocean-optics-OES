d17740aa1227a8a48bf27abeeed7a590abff73f0
Nate George <wordsforthewise@gmail.com>
2015-08-03 15:42:39 -0700

    fixed outlier removal

--- process-data/add Z5-6 and check for Cu3 correlation.py
+++ process-data/add Z5-6 and check for Cu3 correlation.py
@@ -2,7 +2,7 @@ import time, csv
 import pandas as pd
 import numpy as np
 
-def is_outlier(points, thresh=15):
+def is_outlier(points, thresh=5):
     """
     Returns a boolean array with True if points are outliers and False 
     otherwise.
@@ -48,30 +48,36 @@ with open('Y:\Nate\OES\databases\Z5-6 sums.csv','wb') as csvfile:
     sumsWriter = csv.writer(csvfile, delimiter=',')
     sumsWriter.writerow(['substrate','Cu3','DW'] + ['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt) for amnt in comboAmnts])
 
-#print oesDF.columns
 oesByRun = oesDF.groupby(['substrate'])
 for name, group in oesByRun:
     print 'run:',name
     Z5sums = {}
-    outliers = []
     runData = group.groupby(['zone'])
-    print 'expected length 5A, 5B:',len(runData.get_group('5A')['Cu-515']), len(runData.get_group('5B')['Cu-515'])
-    # sometimes one dataset is longer than the other by one
+    #print 'expected length 5A, 5B:',len(runData.get_group('5A')['Cu-515']), len(runData.get_group('5B')['Cu-515'])
+    # sometimes one dataset is longer than the other by one, if try to combine the two with different sizes it gives huge arrays
     if len(runData.get_group('5A')['Cu-515']) != len(runData.get_group('5B')['Cu-515']):
         minIndex = min(len(runData.get_group('5A')['Cu-515']), len(runData.get_group('5B')['Cu-515']))
     else:
         minIndex = len(runData.get_group('5A')['Cu-515'])
     Z5AOESratio = runData.get_group('5A')['Cu-515'][:minIndex]/runData.get_group('5A')['In-451'][:minIndex]
     Z5BOESratio = runData.get_group('5B')['Cu-515'][:minIndex]/runData.get_group('5B')['In-451'][:minIndex]
+    Z5sums['Cu3'] = (runData.get_group('5A')['Cu3'][:minIndex] + runData.get_group('5B')['Cu3'][:minIndex])/2
+    Z5sums['DW'] = (runData.get_group('5A')['DW'][:minIndex] + runData.get_group('5B')['DW'][:minIndex])/2
+    outliers = is_outlier(Z5sums['Cu3'])
     for amnt in comboAmnts:
         Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)] = (amnt*Z5AOESratio + (1-amnt)*Z5BOESratio)
-        Z5sums['Cu3'] = (runData.get_group('5A')['Cu3'][:minIndex] + runData.get_group('5B')['Cu3'][:minIndex])/2
-        Z5sums['DW'] = (runData.get_group('5A')['DW'][:minIndex] + runData.get_group('5B')['DW'][:minIndex])/2
-        outliers.append(is_outlier(Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)]))
-        outliers.append(is_outlier(Z5sums['Cu3']))
-        print len(Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)]), len(Z5sums['Cu3'])
+        outliers = [any(each) for each in zip(outliers,is_outlier(Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)]))]
+        #print len(Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)]), len(Z5sums['Cu3'])
+    
+    # remove outliers
+    outliers = np.array(outliers)
+    print len(Z5sums['Cu3'])
+    Z5sums['Cu3'] = Z5sums['Cu3'][~outliers]
+    Z5sums['DW'] = Z5sums['DW'][~outliers]
+    print len(Z5sums['Cu3'])
+    for amnt in comboAmnts:
+        Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)] = Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)][~outliers]
     with open('Y:\Nate\OES\databases\Z5-6 sums.csv','ab') as csvfile:
         sumsWriter = csv.writer(csvfile, delimiter=',')
         for cnt in range(len(Z5sums['Cu3'])):
-            if cnt not in outliers:
-                sumsWriter.writerow([name,Z5sums['Cu3'][cnt],Z5sums['DW'][cnt]] + [Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)][cnt] for amnt in comboAmnts])
+            sumsWriter.writerow([name,Z5sums['Cu3'][cnt],Z5sums['DW'][cnt]] + [Z5sums['Cu-515/In-451 5A*' + str(amnt) + ' 5B*' + str(1-amnt)][cnt] for amnt in comboAmnts])
