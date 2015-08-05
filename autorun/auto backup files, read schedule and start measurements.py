5f349d036b8018bb3135c77e096aae04f713ef36
Nate George <wordsforthewise@gmail.com>
2015-08-04 17:02:01 -0700

    added backup to primary location

--- autorun/auto backup files, read schedule and start measurements.py
+++ autorun/auto backup files, read schedule and start measurements.py
@@ -19,6 +19,7 @@ sys.stderr = open('Y:/Nate/new MC02 OES program/backup from MC02 computer/data/O
 print '\r\n\r\n***************************\r\n','starting backup check on ', ctime()
  
 nsl.backupFiles('C:/OESdata/','Y:/Nate/new MC02 OES program/backup from MC02 computer/data/')
+nsl.backupFiles('C:/OESdata/','Y:/Experiment Summaries/MC sputter tools OES/data/raw from tool/')
 
 if monitorStart == True:
     subprocess.Popen(['python','C:/OESdata/check if tool sputtering.py',tool])
